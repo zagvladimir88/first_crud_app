3e947c4a5a38e6d17e1047151efb2fa6ba1664cb
Vladimir Zagorny <zagvladimir88@gmail.com>
2022-06-23 15:50:14 +0300

    v1

---
+++ src/main/java/com/crudApp/dao/PersonDAO.java
@@ -0,0 +1,50 @@
+package com.crudApp.dao;
+
+import com.crudApp.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM \"Person\""
+                , new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM \"Person\" WHERE id=?"
+                        , new Object[]{id}
+                        , new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT into \"Person\" VALUES (1,?,?,?)",
+                person.getName(),
+                person.getAge(),
+                person.getEmail());
+    }
+
+    public void update(int id, Person updatePerson) {
+        jdbcTemplate.update("UPDATE \"Person\" SET name=?,age=?,email=? WHERE id=?"
+                , updatePerson.getName(), updatePerson.getAge(), updatePerson.getEmail()
+                , id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM \"Person\" WHERE id=?",id);
+
+    }
+}
